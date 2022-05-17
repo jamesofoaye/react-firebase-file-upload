7b912604778c9a4d99c2a73741b64e0bdc9416e6
James Ofori Ayerakwa <58654469+jamesofoaye@users.noreply.github.com>
2022-05-17 10:08:58 -0700

    changed api keys

--- example/src/util/firebase.js
+++ example/src/util/firebase.js
@@ -7,13 +7,13 @@ import { getStorage } from 'firebase/storage'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBNSbsWuynoRasrDxOz5A_5sUROpipO6A8',
-  authDomain: 'auth.congasgh.com',
-  projectId: 'congas-webapp',
-  storageBucket: 'congas-webapp.appspot.com',
-  messagingSenderId: '883325992993',
-  appId: '1:883325992993:web:e1d8d5bb51862f878be360',
-  measurementId: 'G-1LLQ84RF75',
+  apiKey: '#####################',
+  authDomain: '#####################',
+  projectId: '#####################',
+  storageBucket: '#####################',
+  messagingSenderId: '#####################',
+  appId: '#####################',
+  measurementId: '#####################',
 };
 
 // Initialize Firebase
